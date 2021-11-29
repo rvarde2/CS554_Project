5a3c8cdeedc76142319040b1aa6f21051967b6b6
rvarde2 <rvarde2@uic.edu>
2021-11-29 10:37:24 -0600

    Adding YAML Support

--- Car-Repair/application/build.sbt
+++ Car-Repair/application/build.sbt
@@ -1,1 +1,3 @@
 EclipseKeys.skipProject := false
+// https://mvnrepository.com/artifact/org.yaml/snakeyaml
+libraryDependencies += "org.yaml" % "snakeyaml" % "1.8"
--- Car-Repair/application/src/main/java/com/lightbend/training/carrepair/CarRepairApp.java
+++ Car-Repair/application/src/main/java/com/lightbend/training/carrepair/CarRepairApp.java
@@ -7,11 +7,13 @@ import akka.actor.*;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
+import org.yaml.snakeyaml.Yaml;
 import scala.concurrent.Await;
 import scala.concurrent.duration.Duration;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -29,6 +31,9 @@ public class CarRepairApp implements Terminal{
 
     private final LoggingAdapter log;
 
+    String engine = "Engine";
+    String body = "Body";
+    String wheels = "Wheels";
 
     private final ActorRef carRepair;
 
@@ -83,10 +88,10 @@ public class CarRepairApp implements Terminal{
     }
 
     private void run() throws IOException, TimeoutException, InterruptedException {
-        log.warning(
-            String.format("{} running%nEnter commands into the terminal, e.g. 'q' or 'quit'"),
-            getClass().getSimpleName()
-        );
+//        log.warning(
+//            String.format("{} running%nEnter commands into the terminal, e.g. 'q' or 'quit'"),
+//            getClass().getSimpleName()
+//        );
         commandLoop();//Infinite Loop
         Await.ready(system.whenTerminated(), Duration.Inf());
     }
@@ -98,41 +103,62 @@ public class CarRepairApp implements Terminal{
     }
 
     private void commandLoop() throws IOException{
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            String line = in.readLine();
-            if (line == null) {
-                system.terminate();
-                break;
-            } else {
-                TerminalCommand tc = Terminal.create(line);
-                //Three Options:
-                //1)Create Guest and entertain the request
-                //2)Acquire current status of the system
-                //3)Quit
-                if (tc instanceof TerminalCommand.Guest) {
-                    TerminalCommand.Guest tcg = (TerminalCommand.Guest) tc;
-                    createGuest(tcg.count, tcg.repair, tcg.maxRepairCount);
-                } else if (tc == TerminalCommand.Status.Instance) {
-                    getStatus();
-                } else if (tc == TerminalCommand.Quit.Instance) {
-                    system.terminate();
-                    break;
-                } else {
-                    TerminalCommand.Unknown u = (TerminalCommand.Unknown) tc;
-                    log.warning("Unknown terminal command {}!", u.command);
-                }
+        //BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        Yaml yaml = new Yaml();
+        InputStream inputStream = CarRepairApp.class
+                .getClassLoader()
+                .getResourceAsStream("guestList.yml");
+        for (Object object : yaml.loadAll(inputStream)) {
+            Map<String, Object> guest = (Map<String, Object>) object;
+            String repairString = (String) guest.get("repair");
+            Repair repair;
+            if(repairString.equals(engine)){
+                repair = new Repair.Engine();
+            }else if(repairString.equals(body)){
+                repair = new Repair.Body();
+            }else if(repairString.equals(wheels)){
+                repair = new Repair.Wheels();
             }
-        }
-    }
-
-    protected void createGuest(int count, Repair repair, int maxRepairCount){
-        //Later use YAML for no. of messages
-        for(int i=0;i<count;i++){
+            else{
+                continue;
+            }
             carRepair.tell(new CarRepair.CreateGuest(repair),ActorRef.noSender());
         }
+        while (true) {
+//            String line = in.readLine();
+//            if (line == null) {
+//                system.terminate();
+//                break;
+//            } else {
+//                TerminalCommand tc = Terminal.create(line);
+//                //Three Options:
+//                //1)Create Guest and entertain the request
+//                //2)Acquire current status of the system
+//                //3)Quit
+//                if (tc instanceof TerminalCommand.Guest) {
+//                    TerminalCommand.Guest tcg = (TerminalCommand.Guest) tc;
+//                    createGuest(tcg.count, tcg.repair, tcg.maxRepairCount);
+//                } else if (tc == TerminalCommand.Status.Instance) {
+//                    getStatus();
+//                } else if (tc == TerminalCommand.Quit.Instance) {
+//                    system.terminate();
+//                    break;
+//                } else {
+//                    TerminalCommand.Unknown u = (TerminalCommand.Unknown) tc;
+//                    log.warning("Unknown terminal command {}!", u.command);
+//                }
+//            }
+        }
     }
 
-    protected void getStatus(){
-    }
+//    protected void createGuest(int count, Repair repair, int maxRepairCount){
+//        //Later use YAML for no. of messages
+//        for(int i=0;i<count;i++){
+//            carRepair.tell(new CarRepair.CreateGuest(repair),ActorRef.noSender());
+//        }
+//    }
+//
+//    protected void getStatus(){
+//    }
+
 }
